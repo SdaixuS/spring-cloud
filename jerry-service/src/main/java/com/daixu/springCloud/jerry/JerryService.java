735c73d9f472771a192fa2f4deed1e300877a495
DW-Home <SdaixuS@126.com>
2018-12-08 16:39:15 +0800

    fixed a bug

--- jerry-service/src/main/java/com/daixu/springCloud/jerry/JerryService.java
+++ jerry-service/src/main/java/com/daixu/springCloud/jerry/JerryService.java
@@ -2,13 +2,13 @@ package com.daixu.springCloud.jerry;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringCloudApplication
+@SpringBootApplication
 @RestController
 @RequestMapping("/")
 @RefreshScope
