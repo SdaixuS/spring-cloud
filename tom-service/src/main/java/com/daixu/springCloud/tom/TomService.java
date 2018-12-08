0c87b4aded7b193ebdcae0033e81b97a76b13386
DW-Home <SdaixuS@126.com>
2018-12-08 16:47:15 +0800

    added eureka client in tom and jerry services

--- tom-service/src/main/java/com/daixu/springCloud/tom/TomService.java
+++ tom-service/src/main/java/com/daixu/springCloud/tom/TomService.java
@@ -3,6 +3,7 @@ package com.daixu.springCloud.tom;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 @RefreshScope
+@EnableDiscoveryClient
 public class TomService {
 
   @Value("${sayhi}")
