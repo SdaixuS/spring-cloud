15c6b5fc9986189f2826459a355826f35cbd8a7e
DW-Home <SdaixuS@126.com>
2018-12-08 15:05:46 +0800

    added fields in tom service config

--- tom-service/src/main/java/com/daixu/springCloud/tom/TomService.java
+++ tom-service/src/main/java/com/daixu/springCloud/tom/TomService.java
@@ -1,5 +1,6 @@
 package com.daixu.springCloud.tom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RefreshScope
 public class TomService {
 
+  @Value("${sayhi}")
   public String sayHi;
 
   public static void main(String[] args) {
